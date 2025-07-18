00f7e8a3f5c7e51fefe9cec129bd8300bbb74ec7
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-18 14:16:16 +0200

    [upd + add]index.html (bootstrap classes, div) button generation placeholder [add] style.css (body, .card, etc) [upd + add] script.js (moved axios.get into function, function call for initial load, add button, )

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-const mailListVar = document.getElementById(`mailList`);
-const tempMailListArr = [];
+const mailListVar = document.getElementById("mailList");
+const generateBtn = document.getElementById("generateBtn");
 
-for (let i=0; i<10; i++){
-axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
-	mailListVar.innerHTML += `<li>${resp.data.response}</li>`;
-})
+// Carico le prime 10 email appena apro la pagina
+function loadEmails() {
+  mailListVar.innerHTML = ""; // resetto la lista
+  for (let i = 0; i < 10; i++) {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((resp) => {
+        mailListVar.innerHTML += `<li class="list-group-item">${resp.data.response}</li>`;
+      });
+  }
 }
+
+// Al click del bottone genero nuove email
+generateBtn.addEventListener("click", loadEmails);
+
+// Chiamo la funzione all’avvio per mostrare le mail iniziali
+loadEmails();
